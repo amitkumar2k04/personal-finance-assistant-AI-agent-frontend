3161c21c9a4375fdb13ecd09b25cc1926704f422
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-07-01 01:35:12 +0530

    .

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api/finance';
+const API_BASE_URL = import.meta.env.NODE_ENV === 'production'
+  ? 'https://personal-finance-assistant-ai-agent-teal.vercel.app/api/finance'
+  : 'http://localhost:8080/api/finance';
 
 export const sendMessageToAI = async (userMessage) => {
   try {
@@ -11,3 +13,19 @@ export const sendMessageToAI = async (userMessage) => {
     return 'Sorry, there was an error processing your request.';
   }
 };
+
+
+
+// import axios from 'axios';
+
+// const API_BASE_URL = '/api/finance';
+
+// export const sendMessageToAI = async (userMessage) => {
+//   try {
+//     const response = await axios.post(API_BASE_URL, { question: userMessage });
+//     return response.data.reply;
+//   } catch (error) {
+//     console.error('Error in API call', error);
+//     return 'Sorry, there was an error processing your request.';
+//   }
+// };
